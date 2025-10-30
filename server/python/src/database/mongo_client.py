caf7755756c9809493073960c132ecc454bfbb3c
Angela <angelashu103@gmail.com>
2025-10-30 16:52:37 -0400

    add new info

--- server/python/src/database/mongo_client.py
+++ server/python/src/database/mongo_client.py
@@ -1,11 +1,13 @@
 from pymongo import AsyncMongoClient
 from dotenv import load_dotenv
 import os
+import voyageai
 
 load_dotenv()
 
 client = AsyncMongoClient(os.getenv("MONGO_URI"))
 db = client[os.getenv("MONGO_DB")]
+voyageai.api_key = os.getenv("VOYAGE_API_KEY")
 
 def get_collection(name:str):
     return db[name]
