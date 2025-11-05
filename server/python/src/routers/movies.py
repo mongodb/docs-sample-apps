a5bdec2cc282b98de634157e06d7e7465ee3a629
Jordan Smith <jordan.smith@mongodb.com>
2025-11-04 21:17:01 -0500

    feat: adds aggregation page to the front end

--- server/python/src/routers/movies.py
+++ server/python/src/routers/movies.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Query, Path, Body
 from src.database.mongo_client import db, get_collection
-from src.models.models import CreateMovieRequest, Movie, MovieFilter, SuccessResponse, UpdateMovieRequest, SearchMoviesResponse, BatchUpdateRequest, BatchDeleteRequest
+from src.models.models import CreateMovieRequest, Movie, MovieFilter, SuccessResponse, UpdateMovieRequest, SearchMoviesResponse, VectorSearchResult, BatchUpdateRequest, BatchDeleteRequest
 
 from typing import List
 from datetime import datetime
@@ -472,7 +472,7 @@ async def aggregate_movies_recent_commented(
         # - If querying all movies: return up to 20 results
         # Tip: This prevents overwhelming the client with too much data
         {
-            "$limit": 50 if movie_id else 20
+            "$limit": 20 if movie_id else 5
         },
         # STAGE 7: $project - Shape Final Response Output
         {
